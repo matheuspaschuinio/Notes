5230a6473059a12ace29e8ed70a85bbc7d24634b
Matheus Augusto Paschuinio <matheus.paschuinio@fatec.sp.gov.br>
2025-12-30 10:25:34 -0300

    autenticação

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -37,16 +37,40 @@ class AuthController extends Controller
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        // get all the users from the database
-        // $users = User::all()->toArray();
+        // check if user exists
+        $user = User::where('username', $username)
+                        ->where('deleted_at', NULL)
+                        ->first();
 
-        // as an object instance of the model's class
-        $userModel = new User();
-        $users = $userModel->all()->toArray();
+        if(!$user){
+            return redirect()
+                    ->back()
+                    ->withInput()
+                    ->with('loginError', 'Username or Password incorrect');
+        }
 
-        echo '<pre>';
-        print_r($users);
-    }
+        // check if password is correct
+        if(!password_verify($password, $user->password)){
+            return redirect()
+                    ->back()
+                    ->withInput()
+                    ->with('loginError', 'Username or Password incorrect');
+        }
+
+        // update last login
+        $user->last_login = date('Y-m-d H:i:s');
+        $user->save();
+
+        // login user
+        session([
+            'user' => [
+                'id' => $user->id,
+                'username' => $user->username
+            ]
+            ]);
+
+        echo 'Login com sucesso!';
+    }   
 
     public function logout()
     {
