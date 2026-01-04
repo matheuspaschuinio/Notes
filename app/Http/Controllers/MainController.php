081a2489fbd91b2fc911c6a73761d2e96ebc54a2
Matheus Augusto Paschuinio <matheus.paschuinio@fatec.sp.gov.br>
2026-01-04 09:41:19 -0300

    formatando a data

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -9,9 +10,13 @@ class MainController extends Controller
     public function index()
     {
         // load user's notes
+        $id = session('user.id');
+        $notes = User::find($id)->notes()->get()->toArray(); 
+
+
 
         // show home view
-        return view('home');
+        return view('home', ['notes' => $notes]);
     }
 
     public function newNote()
