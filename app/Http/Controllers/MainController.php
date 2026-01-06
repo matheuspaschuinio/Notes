8c846795143d459abd253c04acfa0fa615499a35
Matheus Augusto Paschuinio <matheus.paschuinio@fatec.sp.gov.br>
2026-01-06 09:17:01 -0300

    edição da nota

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -69,6 +69,58 @@ class MainController extends Controller
     {
         // $id = $this->decryptId($id); 
         $id = Operations::decryptId($id);
+
+        if($id == null){
+            return redirect()->route('home');
+        }
+
+        // load note
+        $note = Note::find($id);
+
+        // show edit note view
+        return view('edit_note', ['note' => $note]);
+    }
+
+    public function editNoteSubmit(Request $request)
+    {
+        // validate request
+        $request->validate(
+            // rules
+            [
+                'text_title' => 'required|min:3|max:200',
+                'text_note' => 'required|min:3|max:3000'
+            ], 
+            // error messages
+            [
+                'text_title.required' => 'O título é obrigatório',
+                'text_title.min' => 'O título deve ter no mínimo :min caracteres',
+                'text_title.max' => 'O título deve ter no máximo :max caracteres',
+
+                'text_note.required' => 'A nota é obrigatória',
+                'text_note.min' => 'A nota deve ter no mínimo :min caracteres',
+                'text_note.max' => 'A nota deve ter no máximo :max caracteres'
+                
+            ]
+        );
+
+        // check if note_id exists
+        if($request->note_id == null){
+            return redirect()->route('home');
+        }
+
+        // decrypt note_id
+        $id = Operations::decryptId($request->note_id);
+
+        // load note
+        $note = Note::find($id);
+
+        // update note
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+
+        // redirect to home
+        return redirect()->route('home');
     }
 
     public function deleteNote($id)
