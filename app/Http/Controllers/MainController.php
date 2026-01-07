bb1c9909c227c0cf540e79a3b9ee00d5a03a0b71
Matheus Augusto Paschuinio <matheus.paschuinio@fatec.sp.gov.br>
2026-01-07 18:16:05 -0300

    delete das notas

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -15,7 +15,11 @@ class MainController extends Controller
     {
         // load user's notes
         $id = session('user.id');
-        $notes = User::find($id)->notes()->get()->toArray(); 
+        $notes = User::find($id)
+                        ->notes()
+                        ->whereNull('deleted_at')
+                        ->get()
+                        ->toArray(); 
 
 
 
@@ -125,7 +129,31 @@ class MainController extends Controller
 
     public function deleteNote($id)
     {
-        // $id = $this->decryptId($id);
         $id = Operations::decryptId($id);
+
+        // load note
+        $note = Note::find($id);
+
+        // show delete note confirmation
+        return view('delete_note', ['note' => $note]);
+    }
+
+    public function deleteNoteConfirm($id)
+    {
+        // check if id is encrypted
+        $id = Operations::decryptId($id);
+
+        // load note
+        $note = Note::find($id);
+
+        // 1. hard delete
+        // $note->delete();
+
+        // 2. soft delete
+        $note->deleted_at = date('Y:m:d H:i:s');
+        $note->save();
+
+        // redirect to home
+        return redirect()->route('home');
     }
 }
